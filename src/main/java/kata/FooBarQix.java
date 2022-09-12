22799762d61095b7b8931ec7acbb0051f7ee6c5d
abhar <abhar@isilis.fr>
2022-09-12 08:43:47 +0200

    Step 2: passing all tests

--- src/main/java/kata/FooBarQix.java
+++ src/main/java/kata/FooBarQix.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class FooBarQix {
 
     private static final Map<Integer, String> divisors = new HashMap<>();
+    public static final String ZERO = "0";
+    public static final String STAR = "*";
 
     static {
         divisors.put(3, "Foo");
@@ -15,34 +17,37 @@ public class FooBarQix {
 
     public String compute(String input) {
 
-
-        String result = divisible(input) + replace(input);
+        String result = divisible(input);
+        result += replace(input, !"".equals(result));
         if (!"".equals(result))
             return result;
 
-        return input.replace("0", "*");
+        return input.replace(ZERO, STAR);
     }
 
     private String divisible(String input) {
+
         StringBuilder result = new StringBuilder();
         for (Integer divisor : divisors.keySet())
             if (Integer.parseInt(input) % divisor == 0) result.append(divisors.get(divisor));
         return result.toString();
     }
 
-    private String replace(String input) {
+    private String replace(String input, boolean changed) {
+
         String[] numbers = input.split("");
         StringBuilder result = new StringBuilder();
         for (String number : numbers) {
             for (Integer divisor : divisors.keySet()) {
                 if (Integer.parseInt(number) == divisor) {
                     result.append(divisors.get(divisor));
+                    changed = true;
                 }
             }
-            if (number.equals("0")) {
-                result.append("*");
+            if (number.equals(ZERO)) {
+                result.append(STAR);
             }
         }
-        return result.toString();
+        return (changed) ? result.toString() : "";
     }
 }
